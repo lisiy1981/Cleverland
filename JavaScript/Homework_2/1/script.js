a6768bb5f37b6a51581e300dfce42a0be3911f11
Aleksei <lisiy.81@gmail.comm>
2022-10-27 17:53:24 +0300

    jsj2

---
+++ JavaScript/Homework_2/1/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let admin;
+
+let userName = "John";
+
+admin = userName;
+
+alert(admin);
---
+++ JavaScript/Homework_2/2/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let yourName = prompt("Как твоё имя?");
+
+alert(`Тебя зовут ${yourName}?`);
+
+let trueName = confirm(`${yourName} ,это правильное имя ?`);
+
+alert(trueName);
